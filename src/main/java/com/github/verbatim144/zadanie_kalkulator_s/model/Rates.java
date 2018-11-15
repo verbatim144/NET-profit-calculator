8aeb76d38c2d54c2db87578975c475f921d3ac01
verbatim144 <vlad061998@gmail.com>
2018-11-15 21:53:50 +0100

    Add Rates class

--- src/main/java/com/github/verbatim144/zadanie_kalkulator_s/model/Rates.java
+++ src/main/java/com/github/verbatim144/zadanie_kalkulator_s/model/Rates.java
@@ -1,4 +1,22 @@
 package com.github.verbatim144.zadanie_kalkulator_s.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+//@JsonIgnoreProperties: to ignore any unknown properties in JSON input without exception
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class Rates {
+
+    private String bid;
+
+    public Rates(){ }
+
+    public String getBid() {
+        return bid;
+    }
+
+    public void setBid(String bid) {
+        this.bid = bid;
+    }
+
+
 }
