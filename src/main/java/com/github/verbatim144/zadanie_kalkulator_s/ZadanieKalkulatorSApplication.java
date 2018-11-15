24894fd9ea3f7051c1f7a2ec1f5c611ca87b9ff1
verbatim144 <vlad061998@gmail.com>
2018-11-15 18:29:57 +0100

    Create empty project

---
+++ src/main/java/com/github/verbatim144/zadanie_kalkulator_s/ZadanieKalkulatorSApplication.java
@@ -0,0 +1,12 @@
+package com.github.verbatim144.zadanie_kalkulator_s;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZadanieKalkulatorSApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZadanieKalkulatorSApplication.class, args);
+	}
+}
