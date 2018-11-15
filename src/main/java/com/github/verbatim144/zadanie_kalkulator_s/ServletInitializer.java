bface228bf50e3ba954668ed4e9effd350f35b7b
verbatim144 <vlad061998@gmail.com>
2018-11-16 00:08:37 +0100

    Add ServletInitializer class

--- src/main/java/com/github/verbatim144/zadanie_kalkulator_s/ServletInitializer.java
+++ src/main/java/com/github/verbatim144/zadanie_kalkulator_s/ServletInitializer.java
@@ -1,4 +1,4 @@
-package com.liaskovsyi.testcalculator;
+package com.github.verbatim144.zadanie_kalkulator_s;
 
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
@@ -7,7 +7,7 @@ public class ServletInitializer extends SpringBootServletInitializer {
 
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(TestcalculatorApplication.class);
+		return application.sources(ZadanieKalkulatorSApplication.class);
 	}
 
 }
