773152e8fcf8e6bf607604c55c236046696ee0a9
verbatim144 <vlad061998@gmail.com>
2018-11-16 00:05:20 +0100

    Update Currency class

---
+++ src/main/java/com/github/verbatim144/zadanie_kalkulator_s/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.liaskovsyi.testcalculator;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(TestcalculatorApplication.class);
+	}
+
+}
---
+++ src/main/java/com/github/verbatim144/zadanie_kalkulator_s/repository/CurrencyRepository.java
@@ -0,0 +1,4 @@
+package com.github.verbatim144.zadanie_kalkulator_s.repository;
+
+public class CurrencyRepository {
+}
