a972ae0a0d960acca337c357e31425dd1adbc6c7
verbatim144 <vlad061998@gmail.com>
2018-11-19 23:47:18 +0100

    Add other little changes in frontend files

--- frontend/src/app/app-routing/app-routing.module.ts
+++ frontend/src/app/app-routing/app-routing.module.ts
@@ -2,10 +2,12 @@
 
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
+import {CalculatorComponent} from "../calculator/calculator.component";
 
 const routes: Routes = [
   {
     path: 'calculator',
+    component: CalculatorComponent
   },
   {
     path: '',
