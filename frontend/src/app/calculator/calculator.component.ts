c359232b13c2e9675cc55a602720bb70ac38254b
verbatim144 <vlad061998@gmail.com>
2018-11-19 22:56:30 +0100

    Add functions connected with salary

--- frontend/src/app/calculator/calculator.component.ts
+++ frontend/src/app/calculator/calculator.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {Currency} from "../currency";
+import {CurrencyService} from "../currency.service";
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-calculator',
@@ -7,9 +10,45 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CalculatorComponent implements OnInit {
 
-  constructor() { }
+  currencies: Currency[];
+  calculator = new Currency();
+  submitted = false;
 
-  ngOnInit() {
+  constructor(private currencyService: CurrencyService,
+              private location: Location) { }
+
+  ngOnInit(): void {
+    this.getSalary();
+  }
+
+  newSalary(): void {
+    this.submitted = false;
+    this.calculator = new Currency();
+  }
+
+  addSalary() {
+    this.submitted = true;
+    this.save();
+  }
+
+  getSalary() {
+    return this.currencyService.getCurrencies()
+      .subscribe(
+        currencies => {
+          console.log(currencies);
+          this.currencies = currencies;
+        }
+      );
+  }
+
+  refresh(): void {
+    window.location.reload();
+  }
+
+  private save(): void {
+    console.log(this.calculator);
+    this.currencyService.addCurrency(this.calculator)
+      .subscribe();
   }
 
 }
