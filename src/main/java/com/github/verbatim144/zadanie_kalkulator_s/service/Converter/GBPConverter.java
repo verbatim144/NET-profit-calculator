cb5215823207c0568fcb27a03c8004bac0be0576
verbatim144 <vlad061998@gmail.com>
2018-11-20 23:52:23 +0100

    Add GBPConverter class

---
+++ src/main/java/com/github/verbatim144/zadanie_kalkulator_s/service/Converter/GBPConverter.java
@@ -0,0 +1,21 @@
+package com.github.verbatim144.zadanie_kalkulator_s.service.Converter;
+
+import com.github.verbatim144.zadanie_kalkulator_s.model.Currency;
+import com.github.verbatim144.zadanie_kalkulator_s.model.Rates;
+import org.springframework.web.client.RestTemplate;
+
+public class GBPConverter implements Converter {
+
+
+    RestTemplate restTemplate = new RestTemplate();
+
+    public Rates GBPrate() {
+        String RateResourceUrl = "http://api.nbp.pl/api/exchangerates/rates/c/gbp/today/";
+        return restTemplate.getForObject(RateResourceUrl, Currency.class).getBidRate();
+    }
+
+    @Override
+    public double convertCurrency(double incomeValue) {
+        return incomeValue * (Double.valueOf(GBPrate().getBid()));
+    }
+}
